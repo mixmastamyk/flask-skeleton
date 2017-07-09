8807cc76d511003c41f27032a3aed673cdede539
Gringo Suave <github@mgmiller.net>
2017-07-09 14:51:30 -0700

    impl: standalone .js, refr: template cleanup

---
+++ src/static/app.js
@@ -0,0 +1,19 @@
+// Common app-wide js goes here.
+'use strict';
+
+
+// code for generic empty tab-modal message dialog.
+function show_msg_dialog(icon, title, body) {
+    $('#msg_dialog .modal-title').html(
+                                    `<i class="fa fa-${icon}"></i> ${title}`);
+    $('#msg_dialog .modal-body').html(`<p>${body}</p>`);
+    $('#msg_dialog').modal('show');
+}
+
+
+
+
+
+
+
+
